dffd6cc37931c6315498e0d0daa6fa72a2fd9252
GabrielBrotas22 <gabrielbrotas@octupus.me>
2021-07-16 10:58:18 -0300

    env

--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const uri = "mongodb://127.0.0.1:27017/catalogbot";
+const uri = process.env.MONGO_URI;
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
